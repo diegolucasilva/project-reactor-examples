337ca7704658a8dfe500968792459c3bca9dc720
Diego Lucas <diego.lucasilva@gmail.com>
2023-10-27 22:02:53 -0300

    generate test

--- src/test/java/com/dls/projectreactorexamples/GenerateOperatorTest.java
+++ src/test/java/com/dls/projectreactorexamples/GenerateOperatorTest.java
@@ -1,2 +1,54 @@
-package com.dls.projectreactorexamples;public class GenerateOperatorTest {
+package com.dls.projectreactorexamples;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.io.*;
+
+public class GenerateOperatorTest {
+
+    @Test
+    public void testFileReadingGenerate(){
+        String filePath = "/Users/diegolucas/Downloads/project-reactor-examples/src/test/resources/example.txt";
+
+        Flux fileFlux = Flux.generate(
+                () -> {
+                    try {
+                        System.out.println("creating bufferedReader");
+                        return new BufferedReader(new FileReader(filePath));
+                    } catch (FileNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                ((bufferedReader, synchronousSink) -> {
+                    try {
+                        String line = bufferedReader.readLine();
+                        if(line !=null){
+                            synchronousSink.next(line);
+                        }else
+                            synchronousSink.complete();
+                    } catch (IOException exception) {
+                        synchronousSink.error(exception);
+                    }
+                    return bufferedReader;
+                }),
+                bufferedReader -> {
+                    try {
+                        System.out.println("End - closing file");
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+        ).log();
+
+        StepVerifier.create(fileFlux)
+                .expectNext("Line 1")
+                .expectNext("Line 2")
+                .expectNext("Line 3")
+                .expectComplete()
+                .verify();
+
+    }
 }
